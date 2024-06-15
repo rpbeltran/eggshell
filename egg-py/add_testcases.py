ff518e762d73d38df789fa10bf21eca0ed372453
Ryan Beltran <rp.beltran@yahoo.com>
2024-06-15 11:55:11 -0700

    runtime: automate runtime_test generation

--- egg-py/add_testcases.py
+++ egg-py/add_testcases.py
@@ -6,6 +6,7 @@ import frontend.lexer_test as lexer_test
 import frontend.parser_test as parser_test
 import frontend.lowering_test as lowering_test
 import backend.py_generator_test as pygen_test
+import runtime.runtime_test as runtime_test
 
 
 here = os.path.dirname(__file__)
@@ -13,6 +14,7 @@ lexer_test_path = f'{here}/frontend/lexer_test.py'
 parser_test_path = f'{here}/frontend/parser_test.py'
 lowering_test_path = f'{here}/frontend/lowering_test.py'
 pygen_test_path = f'{here}/backend/py_generator_test.py'
+runtime_test_path = f'{here}/runtime/runtime_test.py'
 
 
 def make_lexer_test_code(test_name, src) -> str:
@@ -57,6 +59,23 @@ def test_{test_name}():
     assert get_gen_code(src) == expected_gen_code\n"""
 
 
+def make_runtime_test_code(test_name, src, output) -> str:
+    output_lines = output.split('\n')
+    if len(output_lines) > 1:
+        expected_gen_code_inner = '\n'.join(
+            ' ' * 8 + repr(f'\n{line}' if i != 0 else line)
+            for i, line in enumerate(output_lines)
+        )
+        expected_output = f'(\n{expected_gen_code_inner}\n    )'
+    else:
+        expected_output = repr(output)
+    return f"""\n
+def test_{test_name}():
+    src = {repr(src)}
+    expected_output = {expected_output}
+    assert execute_src(src) == expected_output\n"""
+
+
 def main():
     with open(lexer_test_path, 'a') as test_file:
         for name, code in lexer_test.new_test_cases.items():
@@ -76,6 +95,11 @@ def main():
         for name, code in pygen_test.new_test_cases.items():
             code_gen = pygen_test.get_gen_code(code)
             test_file.write(make_backend_test_code(name, code, code_gen))
+
+    with open(runtime_test_path, 'a') as test_file:
+        for name, code in runtime_test.new_test_cases.items():
+            code_gen = runtime_test.execute_src(code)
+            test_file.write(make_runtime_test_code(name, code, code_gen))
 
 
 if __name__ == '__main__':
