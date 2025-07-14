e9e6a617287424404bfdb364cbfc0628981b29b1
Ryan Beltran <rp.beltran@yahoo.com>
2025-07-13 23:44:59 -0700

    Add dfa states

--- eggo/lexer/dfa_states.go
+++ eggo/lexer/dfa_states.go
@@ -9,3 +9,53 @@ func (node StartNode) DebugString() string {
 func (node StartNode) consume(c byte, lexer *Lexer) {
 	// TODO
 }
+
+type OperatorsNode struct{}
+
+func (node OperatorsNode) DebugString() string {
+	return "<OperatorsNode>"
+}
+
+func (node OperatorsNode) consume(c byte, lexer *Lexer) {
+	// TODO
+}
+
+type CommentNode struct{}
+
+func (node CommentNode) DebugString() string {
+	return "<CommentNode>"
+}
+
+func (node CommentNode) consume(c byte, lexer *Lexer) {
+	// TODO
+}
+
+type IdentifierNode struct{}
+
+func (node IdentifierNode) DebugString() string {
+	return "<IdentifierNode>"
+}
+
+func (node IdentifierNode) consume(c byte, lexer *Lexer) {
+	// TODO
+}
+
+type QuotedLiteralNode struct{}
+
+func (node QuotedLiteralNode) DebugString() string {
+	return "<QuotedLiteralNode>"
+}
+
+func (node QuotedLiteralNode) consume(c byte, lexer *Lexer) {
+	// TODO
+}
+
+type QuotedArgListNode struct{}
+
+func (node QuotedArgListNode) DebugString() string {
+	return "<QuotedArgListNode>"
+}
+
+func (node QuotedArgListNode) consume(c byte, lexer *Lexer) {
+	// TODO
+}
