b35493c057dc9a1b65cef31f7909c8af12986689
Ryan Beltran <rp.beltran@yahoo.com>
2025-07-27 00:08:30 -0700

    eggo: Dont use typeids for expressions

--- eggo/parser/ast.go
+++ eggo/parser/ast.go
@@ -1,6 +1,6 @@
 package parser
 
 type SyntaxTree struct {
-	expr     ExpressionType
-	children []SyntaxTree
+	Expr     Expression
+	Children []SyntaxTree
 }
--- eggo/parser/expressions.go
+++ eggo/parser/expressions.go
@@ -1,36 +1,8 @@
 package parser
 
-import "fmt"
-
 type Expression interface {
-	getType() ExpressionType
+	DebugName() string
 
 	accept(parser *Parser) (SyntaxTree, bool)
 	require(parser *Parser) (SyntaxTree, error)
-}
-
-type ExpressionType int
-
-const (
-	Unspecified ExpressionType = iota
-	_TEST_A
-	_TEST_B
-	_TEST_C
-	_TEST_D
-)
-
-func (t ExpressionType) DebugName() string {
-	switch t {
-	case Unspecified:
-		return "Unspecified"
-	case _TEST_A:
-		return "_TEST_A"
-	case _TEST_B:
-		return "_TEST_B"
-	case _TEST_C:
-		return "_TEST_C"
-	case _TEST_D:
-		return "_TEST_D"
-	}
-	panic(fmt.Sprintf("Unknown expression type: %v", t))
 }
